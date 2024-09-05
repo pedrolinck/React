6db54a2c80a0d08fcb303702df16baaa14589f46
pedro_linck@outlook.com <pedro_linck@outlook.com>
2024-09-05 20:37:14 -0300

    adding logic to slide images and stylization

--- Projects/ImageCarousel/Carousel/src/App.jsx
+++ Projects/ImageCarousel/Carousel/src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 
+const photos = [
+  "https://unsplash.com/pt-br/fotografias/estruturas-proximas-ao-corpo-dagua-t2hgHV1R7_g",
+  "https://unsplash.com/pt-br/fotografias/cerca-de-aco-preto-xhnimDJm0CU",
+  "https://unsplash.com/pt-br/fotografias/vista-aerea-fotografia-da-cidade-Dymu1WiZVko"
+]
 function App() {
-  const photos = [
-    "https://unsplash.com/pt-br/fotografias/estruturas-proximas-ao-corpo-dagua-t2hgHV1R7_g",
-    "https://unsplash.com/pt-br/fotografias/cerca-de-aco-preto-xhnimDJm0CU",
-    "https://unsplash.com/pt-br/fotografias/vista-aerea-fotografia-da-cidade-Dymu1WiZVko"
-  ]
 
   const [image, setImage] = useState(0);
 
@@ -15,27 +15,30 @@ function App() {
   }
 
   function handleNextImage(){
-    setImage( image === photos.length - 1 ? - 1 : image - 1)
+    setImage( image === 0 ? photos.length - 1 : image - 1)
   }
 
   return (
-    <>
+    <div>
       <h1>Photo Album</h1>
-      <button className='left-arrow' onClick={handlePrevImage}>
-        {"<<"}
-      </button>
-      <>  
-        {photos.map(
-          (image, index) => photos === index && (
-          <div key={image} className='slide'>
-            <img src={image} alt="images" />
-          </div>
-        ))}
-      </>
-      <button className='right-arrow' onClick={handleNextImage}>
-        {">>"}
-      </button>
-    </>
+      <div className="slider">
+        <button className='left-arrow' onClick={handlePrevImage}>
+          {"<<"}
+        </button>
+        <button className='right-arrow' onClick={handleNextImage}>
+          {">>"}
+        </button>
+        <div>  
+          {photos.map(
+            (image, index) => photos === index && (
+              <div key={image} className='slide'>
+                <img src={image} alt="images" />
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    </div>
   )
 }
 
